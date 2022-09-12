9d1fadf848b78b3c27dafa66a38ca523410e8152
jHenrique <jhenrique.santos@gmail.com>
2022-09-12 19:46:37 -0300

    Finalizando as operações com o JDBC Template.

--- src/main/java/br/com/taocode/vendas/VendasApplication.java
+++ src/main/java/br/com/taocode/vendas/VendasApplication.java
@@ -21,6 +21,7 @@ public class VendasApplication {
 			cliente.setNome("J.Henrique");
 			clienteRepository.salvar(cliente);
 			clienteRepository.salvar(new Cliente("Iwana"));
+			clienteRepository.salvar(new Cliente("Merize"));
 
 			List<Cliente> clientes = clienteRepository.listarTodos();
 
@@ -29,9 +30,34 @@ public class VendasApplication {
 				System.out.printf("Cliente { id=%d, nome='%s' } %n", c.getId(), c.getNome());
 			});
 
+			// Alterando o nome do primeiro registro
+			var clienteBanco = clienteRepository.ler(1);
+			clienteBanco.setNome("José Henrique");
+			clienteRepository.atualizar(clienteBanco);
+
+
 			// Outra forma de listar tudo, mas tem que ter o método toString na classe cliente
+			clientes = clienteRepository.listarTodos();
+			System.out.println("");
 			clientes.forEach(System.out::println);
 
+			// Excluindo o Cliente Merize
+			clienteRepository.apagar(3);
+
+			System.out.println("");
+			clienteRepository.listarTodos().forEach(System.out::println);
+
+			// Apagando tudo
+			clienteRepository.apagar(1);
+			clienteRepository.apagar(2);
+			clientes = clienteRepository.listarTodos();
+
+			if (clientes.isEmpty()) {
+				System.out.printf("%n*** Não há Registros ***");
+			} else {
+				System.out.println("");
+				clientes.forEach(System.out::println);
+			}
 		};
 	}
 
--- src/main/java/br/com/taocode/vendas/domain/repository/ClienteRepository.java
+++ src/main/java/br/com/taocode/vendas/domain/repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package br.com.taocode.vendas.domain.repository;
 
 import br.com.taocode.vendas.domain.entity.Cliente;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -18,15 +19,41 @@ public class ClienteRepository {
 	private JdbcTemplate jdbcTemplate;
 
 	private static final String INSERT = "INSERT INTO CLIENTES (NOME) VALUES (?)";
+	private static final String DELETE = "DELETE FROM CLIENTES WHERE ID=?";
 	private static final String SELECT = "SELECT ID, NOME FROM CLIENTES";
+	private static final String UPDATE = "UPDATE CLIENTES SET NOME=? WHERE ID=?";
+
+	public void apagar (Cliente cliente) {
+		this.apagar(cliente.getId());
+	}
+
+	public void apagar (Integer id) {
+		this.jdbcTemplate.update(DELETE, new Object[] { id });
+	}
+
+	public void atualizar (Cliente cliente) {
+		this.jdbcTemplate.update(UPDATE, new Object[] { cliente.getNome(), cliente.getId() });
+	}
+
+	public Cliente ler (Integer id) {
+		var clienteBanco = this.jdbcTemplate.query(SELECT, obterClienteMapper());
+
+		return new Cliente(clienteBanco.get(0).getId(), clienteBanco.get(0).getNome());
+	}
+
 
 	public List<Cliente> listarTodos () {
-		return this.jdbcTemplate.query(SELECT, new RowMapper<Cliente>() {
+		return this.jdbcTemplate.query(SELECT, obterClienteMapper());
+	}
+
+	@NotNull
+	private RowMapper<Cliente> obterClienteMapper() {
+		return new RowMapper<Cliente>() {
 			@Override
 			public Cliente mapRow(ResultSet resultado, int i) throws SQLException {
 				return new Cliente(resultado.getInt("ID"), resultado.getString("NOME"));
 			}
-		});
+		};
 	}
 
 	public void salvar (Cliente cliente) {
